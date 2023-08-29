65dc52ede72385d96ab1af0207a13aa369f307b8
David Zbarsky <david.zbarsky@airtable.com>
2023-08-29 17:00:23 -0400

    initial commit

---
+++ build.sh
@@ -0,0 +1,5 @@
+set -eux
+
+curl -L "https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.3/$NAME.tar.xz" -o src.tar.xz
+tar -xvf src.tar.xz "$NAME/bin/clang" "$NAME/bin/lld"
+tar -czf "$NAME.tar.xz" "$NAME"
